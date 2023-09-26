54b0f580ce969697e9f08f3c87c086b7404dace5
ParkGu <jump2766@gmail.com>
2023-09-27 00:07:32 +0900

    init: volume으로 연결된 host파일에서 파일 변경을 inotify가 감지 하지 못함

--- docker-setting/inotify/jar_inotifywait.sh
+++ docker-setting/inotify/jar_inotifywait.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 # 감시할 파일 경로
-file_path="/home/spring-api/spring-api-0.0.1-SNAPSHOT.jar"
+file_path="/home/spring-api/jar-path/spring-api-0.0.1-SNAPSHOT.jar"
 
 # inotifywait를 사용하여 파일 이벤트 감시
-inotifywait -m -e create,modify,delete --format "%e %w" "$file_path" |
+inotifywait -m -e create,modify,delete "$file_path" |
 while read -r events file
 do
     case $events in
-#        CREATE)
-#            echo "파일 $file 가 생성되었습니다."
-#            ;;
-#        MODIFY)
-#            echo "파일 $file 가 수정되었습니다."
-#            ;;
-#        DELETE)
-#            echo "파일 $file 가 삭제되었습니다."
-#            ;;
         *)
-            sh /home/spring-api/script/jar_restart.sh
+            echo 'catch'
+#            sh /home/spring-api/script/jar_restart.sh
             ;;
     esac
+done
+
+inotifywait -m -e create,modify -r "/home/spring-api/jar-path/" |
+while read dirname eventlist filename
+do
+  echo ${dirname} ${filename}
 done
--- docker-setting/script/jar_restart.sh
+++ docker-setting/script/jar_restart.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 #jar restart sh 작성
+echo "PID Check..."
+
+CURRENT_PID=$(fuser 8080/tcp)
+
+echo "Running PID: {$CURRENT_PID}"
+
+if [ -z $CURRENT_PID ] ; then
+    echo "Project is not running"
+else
+    kill $CURRENT_PID
+fi
+
+echo "Deploy Project...."
+
+nohup java -Djava.security.egd -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar /home/spring-api/jar-path/spring-api-0.0.1-SNAPSHOT.jar &
+
+echo "Done"
