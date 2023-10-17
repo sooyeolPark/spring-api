a5dccae9635446c5bf200aa2cb8b1397fe89f63c
ParkGu <jump2766@gmail.com>
2023-10-18 00:02:28 +0900

    Init: api 폴더 구조 직관적으로 변경

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
    1. 처음엔 inotify와 같은 파일 감시 시스템을 설치하여 파일을 감시하려 했지만 docker volume의 파일 변경은 컨테이너 내의 파일시스템으로 catch할수 없기때문에 shell script를 이용하였다.
 5. 결론적으로 local에 있는 프로젝트를 빌드하여 jar-path안에 특정 이름의 jar파일이 생성되면 docker 컨테이너 안에 바로 반영이 된다.
 
-6. script파일
+6. script파일 (Window에서는 LF형식으로 변경할 것)
     1. init.sh
        - 컨테이너가 실행되고 아무것도 동작하는 서비스가 없으면 자동으로 컨테이너가 종료된다. 컨테이너가 올라 간 후 jar파일을 실행시켜주고 jar파일의 변경을 감지하는 jar_monioring.sh를 실행시켜주는 쉘스크립트이다.
     2. jar_monitoring.sh
@@ -14,3 +14,7 @@
        - 변경이 감지되면 비교할 jar파일을 바꾸고 jar_restart.sh 쉘 명령어를 실행하고 다시 감시를 시작한다.
     3. jar_restart.sh
        - 현재 jar 실행하고있는 PID를 찾은 후 강제 종료시킨 후 jar를 재시작 해준다.
+
+7. 프로젝트 빌드할 때 java project 기준 ../docker-setting/jar-build에 jar파일이 생성되게 설정한다.
+   1. IntelliJ 기준
+      실행/디버그 구성 -> 새구성 추가 -> Gradle 선택 -> 실행 셀렉트박스에서 Build 선택 후 적용 
---
+++ spring-api/src/main/java/com/jmc/springapi/redis/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package com.jmc.springapi.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean(name = "cacheManager")
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofDays(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(configuration)
+                .build();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
